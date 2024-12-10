91b7896ae39fa5403f8dc894f92e564f1e3b1499
MUKE JOHNBAPTIST <64189841+MUKE-coder@users.noreply.github.com>
2024-12-10 16:59:32 +0300

    updated to next -15

--- components/SimpleFallback.tsx
+++ components/SimpleFallback.tsx
@@ -1,32 +1,18 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+import Lottie from "lottie-react";
 import React from "react";
-
+import LoaderAnimation from "../public/loader.json";
 export default function SimpleFallback() {
   return (
-    <div className="max-w-3xl mx-auto space-y-3 flex flex-col justify-center items-center py-8">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Fallback UI
-      </h1>
-      <div className="">
-        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-balance">
-          The page your looking for does not exist
-        </h2>
+    <div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-[200px] ">
+          <Lottie animationData={LoaderAnimation} />
+        </div>
+        <div className="pt-4">
+          <p className="text-muted-foreground text-sm">Loading...</p>
+        </div>
       </div>
-      <p className="max-w-xl">
-        You must have typed in a wrong address or the page was removed, in the
-        meantime try again or{" "}
-        <Link href={"/"} className="text-blue-600">
-          return to the home page
-        </Link>
-      </p>
-      <Image
-        className="w-96"
-        src="/404.jpg"
-        width={740}
-        height={740}
-        alt="404"
-      />
     </div>
   );
 }
